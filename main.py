ac7554f1f513f458beede8d2dc5295b0f5464f54
mudit816 <72058105+mudit816@users.noreply.github.com>
2021-02-15 18:14:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,37 +16,95 @@ fields=['Dice','X','Y']
 not_impure.append(255)
 not_impure.append(128)
 points_of_impurity=[]
-count_of_image=2
+count_of_image=5
 filename="impure_data_record.csv"
-img_data_temp=Image.open('wafer_image_1.png')
+img_data_temp=Image.open('wafer_image_2.png')
 numpydatatemp=asarray(img_data_temp)
-for k in range(count_of_image):
-    k=k+1
-    st=str(k)
-    str1=name+st+".png"
-    img_data=Image.open(str1)
-    numpydata=asarray(img_data)
+img_data=Image.open('wafer_image_1.png')
+numpydata=asarray(img_data)
+
+
+for i in range(col_count):
+    for j in range(row_count):
+        r=numpydata[i][j][0]
+        b=numpydata[i][j][1]
+        g=numpydata[i][j][2]
+        r1=numpydatatemp[i][j][0]
+        b1=numpydatatemp[i][j][1]
+        g1=numpydatatemp[i][j][2]           
+        flag=False
+        if r==r1 and b==b1 and g==g1:
+            flag=True
+        if flag==False:
+            impure_points=[]
+            impure_points.append(1)
+            impure_points.append(i)
+            impure_points.append(j)
+            points_of_impurity.append(impure_points)
+
+
+for k in range(1,count_of_image-1):
+    img=k+1
+    prev_img=k
+    next_img=img+1
+    str1=name+str(img)+".png"
+    str2=name+str(prev_img)+".png"
+    str3=name+str(next_img)+".png"
+    print(str1)
+    print(str2)
+    print(str3)
+    img_data1=Image.open(str1)
+    img_data2=Image.open(str2)
+    img_data3=Image.open(str3)
+    numpydata1=asarray(img_data1)
+    numpydata2=asarray(img_data2)
+    numpydata3=asarray(img_data3)
     for i in range(col_count):
         for j in range(row_count):
-            r=numpydata[i][j][0]
-            b=numpydata[i][j][1]
-            g=numpydata[i][j][2]
-            #print(r,b,g,"image",k)
-            r1=numpydatatemp[i][j][0]
-            b1=numpydatatemp[i][j][1]
-            g1=numpydatatemp[i][j][2]           
+            curr_r=numpydata1[i][j][0]
+            curr_b=numpydata1[i][j][1]
+            curr_g=numpydata1[i][j][2]
+            prev_r=numpydata2[i][j][0]
+            prev_b=numpydata2[i][j][1]
+            prev_g=numpydata3[i][j][2]
+            next_r=numpydata3[i][j][0]
+            next_b=numpydata3[i][j][1]
+            next_g=numpydata3[i][j][2]
             flag=False
-            if r==r1 and b==b1 and g==g1:
+            if (curr_r==next_r and curr_g==next_g and curr_b==next_b) or (curr_r==prev_r and curr_g==prev_g and curr_b==prev_b):
                 flag=True
             if flag==False:
                 impure_points=[]
                # print(r,b,g)
-                print(r,b,g,"image",k)
-                print(r1,b1,g1,"oldimage")
-                impure_points.append(k)
+                print(curr_r,curr_b,curr_g,"image",k)
+                print(next_r,next_b,curr_g,"oldimage")
+                impure_points.append(img)
                 impure_points.append(i)
                 impure_points.append(j)
                 points_of_impurity.append(impure_points)
+
+img_data_temp=Image.open('wafer_image_4.png')
+numpydatatemp=asarray(img_data_temp)
+img_data=Image.open('wafer_image_5.png')
+numpydata=asarray(img_data)
+for i in range(col_count):
+    for j in range(row_count):
+        r=numpydata[i][j][0]
+        b=numpydata[i][j][1]
+        g=numpydata[i][j][2]
+        r1=numpydatatemp[i][j][0]
+        b1=numpydatatemp[i][j][1]
+        g1=numpydatatemp[i][j][2]           
+        flag=False
+        if r==r1 and b==b1 and g==g1:
+            flag=True
+        if flag==False:
+            impure_points=[]
+            impure_points.append(count_of_image)
+            impure_points.append(i)
+            impure_points.append(j)
+            points_of_impurity.append(impure_points)
+
 with open(filename,'w',newline='') as csvfile:
     csvwriter=csv.writer(csvfile)
     csvwriter.writerows(points_of_impurity)
